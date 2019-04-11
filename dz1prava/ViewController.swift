e7f3ccbaf0465c09b350c78c95d16b8041c7b82d
FIVE <five@josips-macbook-pro.local>
2019-04-11 22:33:55 +0200

    Initial Commit

---
+++ dz1prava/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dz1prava
+//
+//  Created by FIVE on 11/04/2019.
+//  Copyright © 2019 FIVE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
